d92d0a8bcf096181f1f76fa0a7ba8149a4c5598a
a4anddev <a4anddev@gmail.com>
2018-07-04 13:21:35 +0530

    Inner class broadcast receiver method

--- app/src/main/java/c/verbswithexample/broadcastreceiverp/MainActivity.java
+++ app/src/main/java/c/verbswithexample/broadcastreceiverp/MainActivity.java
@@ -1,9 +1,13 @@
 package c.verbswithexample.broadcastreceiverp;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,4 +23,26 @@ public class MainActivity extends AppCompatActivity {
         sendBroadcast(intent);
 
     }
+
+    public void ThirdReceiverInnerClassMethod(View view) {
+        Intent intent = new Intent(this, MyThirdReceiverInnerClass.class);
+        sendBroadcast(intent);
+
+
+    }
+
+    public static class MyThirdReceiverInnerClass extends BroadcastReceiver{
+
+        private static final String TAG = MyThirdReceiverInnerClass.class.getSimpleName();
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+
+            Log.i(TAG, "My Third Receiver Class");
+            Toast.makeText(context, "My Third Receiver Class", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
 }
