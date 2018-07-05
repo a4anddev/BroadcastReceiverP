86d790e7f7d7c361c36f027253d738ef8b92ee44
a4anddev <a4anddev@gmail.com>
2018-07-05 12:02:44 +0530

    dynamic register with event mandatory few event

--- app/src/main/java/c/verbswithexample/broadcastreceiverp/MainActivity.java
+++ app/src/main/java/c/verbswithexample/broadcastreceiverp/MainActivity.java
@@ -8,17 +8,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private MyFirstReceiver myFirstReceiver;
+    private TextView tv;
+    private int ctr = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myFirstReceiver = new MyFirstReceiver();
+        tv = findViewById(R.id.tv);
 
     }
 
@@ -42,4 +47,29 @@ public class MainActivity extends AppCompatActivity {
         // we  have to unregister when we close app otherwise will get error
         unregisterReceiver(myFirstReceiver);
     }
+
+    public void myRegisterBroadcast(View view) {
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_TIME_TICK);
+        registerReceiver(timeTickReceive, intentFilter);
+    }
+
+    public void myUnRegisterBroadcast(View view) {
+
+        unregisterReceiver(timeTickReceive);
+    }
+
+
+    private BroadcastReceiver timeTickReceive = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            int minute = ctr;
+            tv.setText(minute + " minute Over ");
+            ctr++;
+            Toast.makeText(context, "Receive Broadcast", Toast.LENGTH_SHORT).show();
+
+        }
+    };
 }
