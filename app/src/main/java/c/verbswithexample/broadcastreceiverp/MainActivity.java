26c1b38c69da451e8bbf55a7fcafac7afe224a77
a4anddev <a4anddev@gmail.com>
2018-07-05 14:31:48 +0530

    sticky Broadcast receiver

--- app/src/main/java/c/verbswithexample/broadcastreceiverp/MainActivity.java
+++ app/src/main/java/c/verbswithexample/broadcastreceiverp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,62 +15,38 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private MyFirstReceiver myFirstReceiver;
-    private TextView tv;
-    private int ctr = 1;
 
-    @Override
+       @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myFirstReceiver = new MyFirstReceiver();
-        tv = findViewById(R.id.tv);
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
 
-        // this application on running state that time work receiver
-        IntentFilter intentFilter = new IntentFilter();
-    // both are similar use one
-        intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
-//        intentFilter.addAction("android.intent.action.AIRPLANE_MODE");
+    public void methodOne(View view) {
 
-        registerReceiver(myFirstReceiver, intentFilter);
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // we  have to unregister when we close app otherwise will get error
-        unregisterReceiver(myFirstReceiver);
-    }
-
-    public void myRegisterBroadcast(View view) {
-
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(Intent.ACTION_TIME_TICK);
-        registerReceiver(timeTickReceive, intentFilter);
-    }
-
-    public void myUnRegisterBroadcast(View view) {
-
-        unregisterReceiver(timeTickReceive);
-    }
+           IntentFilter intentFilter = new IntentFilter();
+           intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
 
 
-    private BroadcastReceiver timeTickReceive = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
+           Intent intent = registerReceiver(null, intentFilter);
 
-            int minute = ctr;
-            tv.setText(minute + " minute Over ");
-            ctr++;
-            Toast.makeText(context, "Receive Broadcast", Toast.LENGTH_SHORT).show();
+           int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+           if (status == BatteryManager.BATTERY_STATUS_CHARGING){
 
+               Toast.makeText(this, "Bettery Getting Charged", Toast.LENGTH_SHORT).show();
+           }
+
+        if (status == BatteryManager.BATTERY_STATUS_DISCHARGING){
+
+            Toast.makeText(this, "Bettery Getting Discharged", Toast.LENGTH_SHORT).show();
         }
-    };
+
+        if (status == BatteryManager.BATTERY_STATUS_FULL){
+
+            Toast.makeText(this, "Bettery Fully Charged", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 }
